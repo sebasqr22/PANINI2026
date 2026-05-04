b4072f6acd2a54ceec2c20cc9d00058892615e97
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2026-05-04 11:48:55 -0600

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Replace this with your actual Render backend URL after deploy
-  apiUrl: 'https://panini2026-api.onrender.com'
+  apiUrl: 'https://panini2026-backend.onrender.com'
 };
